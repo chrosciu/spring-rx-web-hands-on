99c55a4f92d7f6685c02d303f519b664bc980dc3
Marcin Chrost <marcin.chrost@jcommerce.pl>
2019-05-22 10:00:53 +0200

    Test for UserGithubRouter

--- src/test/java/com/chrosciu/rxweb/util/Users.java
+++ src/test/java/com/chrosciu/rxweb/util/Users.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Users {
-    public static final User JANUSZ = User.builder().id("1").firstName("Janusz").lastName("Cebulak").build();
-    public static final User MIREK = User.builder().id("2").firstName("Mirek").lastName("Handlarz").build();
+    public static final User JANUSZ = User.builder().id("1").firstName("Janusz").lastName("Cebulak").githubName("chrosciu").build();
+    public static final User MIREK = User.builder().id("2").firstName("Mirek").lastName("Handlarz").githubName("octocat").build();
 }
