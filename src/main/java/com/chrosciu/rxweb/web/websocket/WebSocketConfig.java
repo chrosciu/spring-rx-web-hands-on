c867e3e0fd5463bda390059bc1bef9d85de087fa
Marcin Chrost <marcin.chrost@jcommerce.pl>
2019-07-05 14:26:13 +0200

    Removed integration tests for web sockets

--- src/main/java/com/chrosciu/rxweb/web/websocket/WebSocketConfig.java
+++ src/main/java/com/chrosciu/rxweb/web/websocket/WebSocketConfig.java
@@ -6,11 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.reactive.socket.WebSocketHandler;
-import org.springframework.web.reactive.socket.server.RequestUpgradeStrategy;
-import org.springframework.web.reactive.socket.server.WebSocketService;
-import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService;
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
-import org.springframework.web.reactive.socket.server.upgrade.JettyRequestUpgradeStrategy;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -37,15 +33,4 @@ public class WebSocketConfig {
     public WebSocketHandlerAdapter handlerAdapter() {
         return new WebSocketHandlerAdapter();
     }
-
-    @Bean
-    public RequestUpgradeStrategy requestUpgradeStrategy() {
-        return new JettyRequestUpgradeStrategy();
-    }
-
-    @Bean
-    public WebSocketService webSocketService() {
-        return new HandshakeWebSocketService(requestUpgradeStrategy());
-    }
-
 }
