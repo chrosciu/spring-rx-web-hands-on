4c3a8a25782342419e9af4b79e956ec1967aedd6
Marcin Chrost <marcin.chrost@jcommerce.pl>
2019-07-05 14:35:40 +0200

    Added WebSockets JS example

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -11,4 +11,24 @@ source.onmessage = function(event) {
     document.getElementById("result").innerHTML += event.data + "<br>";
 };
 
+var socket = new WebSocket('ws://localhost:8080/ws/ticks');
+
+socket.onopen = function(evt) {
+    console.log("Opened...", evt);
+    socket.send("2");
+    console.log("Message is sent...");
+};
+
+socket.onmessage = function (evt) {
+    console.log("Message is received...", evt);
+};
+
+socket.onerror = function (err) {
+    console.log("Error...", err);
+};
+
+socket.onclose = function (evt) {
+    console.log("Closed...", evt);
+};
+
 
