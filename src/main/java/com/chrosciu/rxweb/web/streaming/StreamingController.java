1ed2c3953e901dcb2a36cf4f89d70fb1276b5cad
Marcin Chrost <mchrost82@gmail.com>
2023-10-09 22:54:52 +0200

    StreamingController - solution

--- src/main/java/com/chrosciu/rxweb/web/streaming/StreamingController.java
+++ src/main/java/com/chrosciu/rxweb/web/streaming/StreamingController.java
@@ -1,9 +1,17 @@
 package com.chrosciu.rxweb.web.streaming;
 
+import com.chrosciu.rxweb.model.User;
 import com.chrosciu.rxweb.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+import static org.springframework.http.MediaType.APPLICATION_NDJSON_VALUE;
+import static org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE;
 
 @RestController
 @RequestMapping("/streaming")
@@ -13,7 +21,23 @@ public class StreamingController {
 
     //TODO: Delay as query param
 
-    //TODO: Stream all users in 3 ways - as event stream, as SSE and as ND JSON
+    @GetMapping(value = "/users")
+    public Flux<User> getAllUsers() {
+        return userRepository.findAll()
+                .delayElements(Duration.ofSeconds(0));
+    }
+
+    @GetMapping(value = "/users/sse", produces = TEXT_EVENT_STREAM_VALUE)
+    public Flux<User> getAllUsersAsSse() {
+        return userRepository.findAll()
+                .delayElements(Duration.ofSeconds(0));
+    }
+
+    @GetMapping(value = "/users/ndjson", produces = APPLICATION_NDJSON_VALUE)
+    public Flux<User> getAllUsersAsNdJson() {
+        return userRepository.findAll()
+                .delayElements(Duration.ofSeconds(0));
+    }
 
 
 }
