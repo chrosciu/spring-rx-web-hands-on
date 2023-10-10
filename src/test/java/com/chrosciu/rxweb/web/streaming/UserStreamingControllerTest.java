4b564410e5c48c1402d3a0da1fce2bc82662b168
Marcin Chrost <mchrost82@gmail.com>
2023-10-10 20:58:36 +0200

    Read full SSE in test using type reference

--- src/test/java/com/chrosciu/rxweb/web/streaming/UserStreamingControllerTest.java
+++ src/test/java/com/chrosciu/rxweb/web/streaming/UserStreamingControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -64,6 +66,17 @@ public class UserStreamingControllerTest {
                 .bodyToFlux(User.class);
 
         assertUsers(users);
+    }
+
+    @Test
+    void shouldStreamUsersAsSseReadFullSse() {
+        ParameterizedTypeReference<ServerSentEvent<User>> typeReference = new ParameterizedTypeReference<>() {};
+        Flux<ServerSentEvent<User>> usersSse = webClient.get()
+                .uri("/streaming/users/sse")
+                .retrieve()
+                .bodyToFlux(typeReference);
+
+        assertUsers(usersSse.map(ServerSentEvent::data));
     }
 
     @Test
